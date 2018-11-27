02516497929623cd599d2704ded1764b0a33cfcb
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-11-27 13:35:47 -0200

    Creating actions

---
+++ src/store/actions/searchVideo.js
@@ -0,0 +1,39 @@
+import youtubeSearch from 'youtube-api-v3-search'
+import ApiKey from '../../../.env'
+
+
+export const beginSearchVideo = () => {
+  return {
+    type: 'SEARCH_VIDEO_BEGIN',
+    loading: true,
+    erro: false
+  }
+}
+
+export const searchVideoSuccess = (videos) => {
+  return {
+    type: 'SEARCH_VIDEO_SUCCESS',
+    videos,
+    loading: false,
+    erro: false
+  }
+}
+
+export const searchVideoError = () =>  {
+  return {
+    type: 'SEARCH_VIDEO_ERROR',
+    error: true,
+    loading:false
+  }
+}
+
+export const searchByName = (name) => {
+  return dispatch => {
+    dispatch(beginSearchVideo())
+    youtubeSearch(ApiKey,{q:name})
+    .then(data => dispatch(searchVideoSuccess(data.items)))
+    .catch(() => {
+      dispatch(searchVideoError())
+    })
+  }
+}
