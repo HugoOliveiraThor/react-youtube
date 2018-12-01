dc7231caeffaabc391671c117ebe1105d3a055c1
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-12-01 09:18:56 -0200

    Add the last snippets

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -27,7 +27,6 @@ class VideoPlayer extends Component {
   }
 
   render () {
-    console.log('props', this.props)
     return (
       <div className="video-player">
         {this.props.video.id ?  this.renderVideo() : this.renderAdvertisement()}
